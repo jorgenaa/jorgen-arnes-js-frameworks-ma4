5af13459df22c43651d27f31782eb1f07795050a
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2020-12-11 14:33:21 +0100

    created the recipeList component that fetch data from an API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import RecipeList from './components/RecipeList';
 
 function App() {
   return (
     <div className="App">
-     
+        <RecipeList />
     </div>
   );
 }
